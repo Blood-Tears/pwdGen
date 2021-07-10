72d9133e550e704af19c8f044ff324b28c4d3fed
Blood-Tears <59869571+Blood-Tears@users.noreply.github.com>
2021-07-10 16:02:24 +0100

    Add files via upload

---
+++ pwdgen.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+app = input("Welcome. Please enter the name of the platform: ")
+username = input("Please enter your username: ")
+addrs = input("Enter the Email address: ") 
+#----------------------------Password---------------------------
+lenght = int(input("How many characters would you like in your password: "))
+l = string.ascii_lowercase
+u = string.ascii_uppercase
+p = string.punctuation
+d = string.digits
+chars = l+u+p+d
+pwd = "".join((random.choice(chars) for i in range(lenght)))
+
+#----------------------------File Handling----------------------
+file = open("/home/bloodtears/Documents/Code/Python/pwdgen/passwords/" +app+".txt", "a")
+file.write("Platform: "+app+"\n"
+"Username: "+username+"\n"
+"Address: "+addrs+"\n"
+"password: "+pwd+"\n"
+)
+file.close()
+
+print("===================================================================")
+print("Alright! we have saved all of your information in "+app+".txt")
+file = open("/home/bloodtears/Documents/Code/Python/pwdgen/passwords/" +app+".txt", "r")
+print(file.read())
+print("===================================================================")
