bbac4cc034d2e2721056ca32767c9161cf3f3055
Blood-Tears <59869571+Blood-Tears@users.noreply.github.com>
2021-07-11 19:40:25 +0100

    Update pwdgen.py

--- pwdgen.py
+++ pwdgen.py
@@ -1,22 +1,22 @@
 import random
 import string
 
-directory = input("Welcome! First, type down the path to where the files will be created: ")
+path = input("Welcome! First, type down the path to the file: ")
 app = input("Please enter the name of the platform: ")
 username = input("Please enter your username: ")
-addrs = input("Enter the Email address: ") 
+addrs = input("Enter your Email address: ")
 #----------------------------Password---------------------------
-lenght = int(input("How many characters would you like in your password: "))
+length = int(input("How many characters would you like in your password: "))
 l = string.ascii_lowercase
 u = string.ascii_uppercase
 p = string.punctuation
 d = string.digits
 chars = l+u+p+d
-pwd = "".join((random.choice(chars) for i in range(lenght)))
+pwd = "".join((random.choice(chars) for i in range(length)))
 
 #----------------------------File Handling----------------------
-file = open(directory+app+".txt", "a")
-file.write("Platform: "+app+"\n"
+file = open(path + "/" + app + ".txt", "a")
+file.write("Platform: " + app + "\n"
 "Username: "+username+"\n"
 "Address: "+addrs+"\n"
 "Password: "+pwd+"\n"
@@ -24,7 +24,9 @@ file.write("Platform: "+app+"\n"
 file.close()
 
 print("===================================================================")
-print("Alright! we have saved all of your information in "+app+".txt")
-file = open(directory+app+".txt", "r")
-print(file.read())
+print("Alright! We have saved all of your information in " + app + ".txt \n")                                                                       
+print("Platform: " + app + "\n"
+"Username: "+username+"\n"
+"Address: "+addrs+"\n"
+"Password: "+pwd)
 print("===================================================================")
